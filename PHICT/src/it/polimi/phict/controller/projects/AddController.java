0ce208f2b18adae025b927d960ea817416cfaf3b
IcyTotem <nicolo1990@yahoo.it>
2012-06-25 21:56:22 +0200

    Fix.

--- PHICT/src/it/polimi/phict/controller/projects/AddController.java
+++ PHICT/src/it/polimi/phict/controller/projects/AddController.java
@@ -1,5 +1,10 @@
 package it.polimi.phict.controller.projects;
 
+import java.util.*;
+
+import it.polimi.phict.model.*;
+import it.polimi.phict.service.*;
+
 import org.slim3.controller.Controller;
 import org.slim3.controller.Navigation;
 
@@ -7,6 +12,8 @@ public class AddController extends Controller {
 
     @Override
     public Navigation run() throws Exception {
+        List<Theme> themes = ThemeManagerService.get().selectAll();
+        requestScope("themes", themes);
         return forward("add.jsp");
     }
 }
